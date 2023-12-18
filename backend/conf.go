474a920b01c983fdc6b02f2d7797da19630d8a8b
Harisankar R <rhari.s7@gmail.com>
2023-12-18 11:57:50 +0530

    [Backend] Refactor main to move secrets to separte file

---
+++ backend/conf.go
@@ -0,0 +1,17 @@
+package main
+
+// Azure CLU configurations
+var AZURE_OCP_APIM_SUBSCRIPTION_KEY string = ""
+var AZURE_APIM_REQUEST_ID string = ""
+var AZURE_CLU_URL string = ""
+var AZURE_CLU_KIND string = ""
+var AZURE_CLU_PROJECT_NAME string = ""
+var AZURE_CLU_DEPLOYMENT_NAME string = ""
+var AZURE_CLU_STRING_INDEX_TYPE string = ""
+var AZURE_CLU_INTENT_THRESHOLD float32 = 0.85
+var AZURE_REQUIRED_INTENT string = ""
+
+// ChatGPT API configurations
+var CHATGPT_API_KEY string = ""
+var CHATGPT_MODEL string = ""
+var CHATGPT_URL string = ""
--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,39 @@ package main
 
 import (
 	"alle/client/azure"
-	"fmt"
+	chatgpt "alle/client/chatGPT"
+	"alle/routes"
+	"log"
+	"net/http"
 )
 
 func main() {
 
-	fmt.Print("Hello world")
+	azureCLUClient := azure.AzureCLUClient{
+		APIMSubscriptionKey: AZURE_OCP_APIM_SUBSCRIPTION_KEY,
+		APIMRequestId:       AZURE_APIM_REQUEST_ID,
+		URL:                 AZURE_CLU_URL,
+		Kind:                AZURE_CLU_KIND,
+		ProjectName:         AZURE_CLU_PROJECT_NAME,
+		DeploymentName:      AZURE_CLU_DEPLOYMENT_NAME,
+		StringIndexType:     AZURE_CLU_STRING_INDEX_TYPE,
+		Threshold:           AZURE_CLU_INTENT_THRESHOLD,
+		RequiredIntent:      AZURE_REQUIRED_INTENT,
+	}
 
-	azureCLU := azure.AzureCLU{}
-	x, y, z := azureCLU.GetIntentAndEntity("Get me pictures of dog from my saved data")
-	fmt.Printf("%t %s %s", x, y, z)
+	chatgptClient := chatgpt.ChatGPTClient{
+		ApiKey:  CHATGPT_API_KEY,
+		Model:   CHATGPT_MODEL,
+		URL:     CHATGPT_URL,
+		Context: make([]chatgpt.GPTMesssage, 0),
+	}
+
+	router := routes.Router(chatgptClient, azureCLUClient)
+
+	log.Println("Server running at port 8080")
+	err := http.ListenAndServe(":8080", routes.CorsMiddleware(router))
+	if err != nil {
+		log.Fatalf("There's an error with the server, %s", err)
+	}
 
 }
